cc6edf967090bdb069b069bf595642ac789d2de2
ben <ben12505ca@gmail.com>
2025-05-06 17:45:54 +0800

    add CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from app.api.main import api_router
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允許所有來源
+    allow_credentials=True,
+    allow_methods=["*"],  # 允許所有方法
+    allow_headers=["*"],  # 允許所有請求頭
+)
 
 app.include_router(api_router, prefix="/api/v1")
 app.mount("/static", StaticFiles(directory="/app/app/static"), name="static")
